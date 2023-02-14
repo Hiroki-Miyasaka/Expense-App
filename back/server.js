77861dcfa61c8aa57ae332fbd413abf467aaa0b6
Hiroki <h.m.jojo.8258@gmail.com>
2023-02-13 23:15:42 -0800

    first commit

---
+++ back/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/health", (req, res) => {
+    res.json({
+        status: "ok",
+        author: "Hiroki",
+        app: "Expense"
+    });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is running on port" + process.env.PORT + "...");
+});
